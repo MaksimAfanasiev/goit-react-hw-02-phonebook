e0a0ab7ec10f05da88ccee35cd34bc8d7e2ddc0a
MaksimAfanasiev <m_afanasyev@ukr.net>
2022-07-10 03:52:41 +0300

    Task 1-3
    Add Tasks 1-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,63 @@
 import { Component } from "react";
 
+import { nanoid } from 'nanoid'
+import {Form} from "./Form/Form"
+
 export class App extends Component {
-  render() {
-    return (<div>
-      Hello World
-    </div>)
+  state = {
+    contacts: [],
+    filter: '',
+    name: '',
+    number: ''
   }
+
+  onInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  addContact = (name, number) => {
+
+    this.setState(({contacts}) => {
+      return {
+        contacts: [...contacts, {
+          id: nanoid(),
+          name: name,
+          number: number
+        }],
+      }
+    })
+  }
+  
+  render() {
+
+    const visibleContacts = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(this.state.filter.toLowerCase()));
+
+    return (
+      <>
+        <h1>Phonebook</h1>
+
+        <Form addContact={this.addContact} />
+
+        <h2>Contacts</h2>
+
+        <label>
+          Find contacts by name
+          <input type="text" name="filter" value={this.state.filter} onChange={this.onInputChange} />
+        </label>
+
+        <ul>
+          {visibleContacts.map(contact => {
+            return (
+              <li key={contact.id}>{contact.name}: {contact.number}</li>
+            )
+          })}
+        </ul>
+
+      </>
+    )
+  }
+
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,63 @@
+import { Component } from "react";
+
+export class Form extends Component {
+
+    state = {
+        name: '',
+        number: ''
+    }
+
+    onInputChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+        [name]: value,
+        })
+    }
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        
+        this.props.addContact(this.state.name, this.state.number);
+
+        this.setState({
+            name: '',
+            number: ''
+        })
+    }
+
+    render() {
+        return (
+        <form onSubmit={this.onFormSubmit}>
+
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={this.state.name}
+              onChange={this.onInputChange}
+            />
+          </label>
+
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={this.state.number}
+              onChange={this.onInputChange}
+            />
+          </label>
+
+          <button type="submit">Add contact</button>
+
+        </form>
+        )
+    }
+}
